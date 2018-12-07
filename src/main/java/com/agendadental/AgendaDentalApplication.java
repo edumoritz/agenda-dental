7d5ffd322bb4497b9fa49b65d83430efba350d1f
edumoritz <eduardo.moritz@hotmail.com>
2018-12-06 23:09:27 -0200

    first commit

---
+++ src/main/java/com/agendadental/AgendaDentalApplication.java
@@ -0,0 +1,12 @@
+package com.agendadental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDentalApplication.class, args);
+	}
+}
