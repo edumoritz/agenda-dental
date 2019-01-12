4273ab7e0e57ce537c2b21cd9dd9f01d7bedf95c
edumoritz <eduardo.moritz@hotmail.com>
2019-01-12 15:09:30 -0200

    editando e excluindo

--- src/main/java/com/agendadental/controller/PacientesController.java
+++ src/main/java/com/agendadental/controller/PacientesController.java
@@ -1,10 +1,13 @@
 package com.agendadental.controller;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -37,6 +40,26 @@ public class PacientesController {
 		pacienteService.salvar(paciente);
 		attr.addFlashAttribute("success", "Paciente inserido com sucesso.");
 		return "redirect:/pacientes/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("paciente", pacienteService.buscarPorId(id));
+		return "pacientes/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Paciente paciente, RedirectAttributes attr) {
+		pacienteService.editar(paciente);
+		attr.addFlashAttribute("success", "Paciente editado com sucesso.");
+		return "redirect:/pacientes/cadastrar";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		pacienteService.excluir(id);
+		attr.addFlashAttribute("success", "Paciente excluido com sucesso.");
+		return "redirect:/pacientes/listar";
 	}
 	
 	@ModelAttribute("ufs")
